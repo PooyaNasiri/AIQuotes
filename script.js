32302b16948b6c450dd3ee9a8570e85f8a7ba1b0
Pooya Nasiri <pooya.nasiri75@gmail.com>
2025-11-18 14:45:09 +0000

    Update GAS_URL to the correct deployed Google Apps Script web app URL

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Replace the placeholder below with your deployed Google Apps Script web app URL.
-const GAS_URL = 'https://script.google.com/macros/s/AKfycbxWz20_NiyTdJ_n1VwdQVdo0_jL8M-E3AaNx1FuuGYW0wQdQ8zRpmgBzQYEpBrmNAaReQ/exec';
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbxvu4pgPyvx-N3FbGptzCzr411tmUdjnEZUzLr9XO0Bq5wG5r48hFTY0x7d7xkFittp0Q/exec';
 const btn = document.getElementById('generate');
 const quoteEl = document.getElementById('quote');
 
